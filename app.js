0c977880cd04576e89f9702f97daf8373419973d
Ubah Johnpaul Oluchukwu <ubahjohnpaul52@gmail.com>
2025-12-18 12:56:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+// const Post1 = require('./models/postModel')
+const postRouter = require('./routes/postRoutes')
+const ProdctRouter = require('./routes/productRoute')
+
+
+const app = express()
+
+
+connectDB()
+
+
+
+// middlewares
+app.use(express.json())// helps us to receive json data into this app
+app.use(express.urlencoded({extended:false}))
+app.use('/',postRouter)
+app.use('/',ProdctRouter)
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+// app.get('/win',async(req,res)=>{
+//     const info = await Post1.find()
+//     res.send(info)
+// })
+const port = process.env.PORT || 7000
+
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`)
+})
